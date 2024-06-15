3a23a96133db53a19e15b3cd29a7cff1219dd66e
Faisal Abdirashid <anzifai6387@gmail.com>
2024-06-15 14:00:48 +0300

    Add Eureka Discovery Server
    Added the Discovery Server (Eureka) to kick start the disaster Alert application.

---
+++ ds-eureka/src/main/kotlin/com/disaster/ds_eureka/DsEurekaApplication.kt
@@ -0,0 +1,11 @@
+package com.disaster.ds_eureka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DsEurekaApplication
+
+fun main(args: Array<String>) {
+	runApplication<DsEurekaApplication>(*args)
+}
---
+++ ds-eureka/src/test/kotlin/com/disaster/ds_eureka/DsEurekaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.disaster.ds_eureka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DsEurekaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
