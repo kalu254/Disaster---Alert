1b081938ad41276325f056e15412276f767097e4
Faisal Abdirashid <anzifai6387@gmail.com>
2024-06-15 14:17:07 +0300

    add configuration for dev enviroment.
    Added the requires code to startup the Eureka server in a local configuration .
    The local port set is 8761

--- ds-eureka/src/main/kotlin/com/disaster/ds_eureka/DsEurekaApplication.kt
+++ ds-eureka/src/main/kotlin/com/disaster/ds_eureka/DsEurekaApplication.kt
@@ -2,8 +2,10 @@ package com.disaster.ds_eureka
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class DsEurekaApplication
 
 fun main(args: Array<String>) {
